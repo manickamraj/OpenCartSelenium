a024cc386aaf7e3a102e4ec589aaf2d0885fd0e2
manickamraj <bmanickamraj@gmail.com>
2023-09-04 17:44:50 +0530

    Update TestDriver.java

--- src/test/java/TestDriver.java
+++ src/test/java/TestDriver.java
@@ -20,6 +20,8 @@ public class TestDriver {
 			System.out.println("Provide either Chrome or Edge browser");
 		
 	}
+		System.out.println("Update done by MANICKAM");
+		System.out.println("Update done by BADHRACHALAM");
 		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
